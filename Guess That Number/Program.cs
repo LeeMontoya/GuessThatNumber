f17dc5996052af5dc29f86f31c4eda8927c4b83b
LeeMontoya <jleemontoya@gmail.com>
2014-04-17 16:59:29 -0600

    Finished
    Game is playable

---
+++ Guess That Number/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guess_That_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+
+            int val = r.Next(1, 100);
+            int guess = 0;
+            bool correct = false;
+            int count = 0;
+            
+            Console.WriteLine("I'm thinking of a number between 1 and 100.");
+
+            while (!correct)
+            {
+                Console.Write("Guess: ");
+                string input = Console.ReadLine();
+                count++;
+
+                if (!int.TryParse(input, out guess))
+                {
+                    Console.WriteLine("Is that a number? No. Duh! You silly goose. Try again!");
+                    continue;
+                }
+
+                if (guess < val)
+                {
+                    Console.WriteLine("No, the number I'm thinking of is higher than that number.");
+                }
+                else if (guess > val)
+                {
+                    Console.WriteLine("No, the number I'm thinking of is lower than that number.");
+                }
+                else
+                {
+                    correct = true;
+                    Console.WriteLine("You guessed right! It took you " + count + " guess.");
+                    Console.ReadKey();
+                }
+            } 
+            
+                     
+        }
+    }
+}
